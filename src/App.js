e4204a8a1e59af11349a4c46afc71552be76afbe
DARSHON CRUDUP <dlc756@yahoo.com>
2023-04-06 13:55:34 -0400

    css update to home with nav

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Nav from './Nav';
 import ParkData from './ParkData';
 import DropDownList from './DropDownList';
-import Footer from './Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import About from './About';
+import "./App.css";
+import Button from 'react-bootstrap/Button';
 import Login from "./Login";
 import Logout from "./Logout";
 import Profile from "./Profile";
+import Footer from './Footer';
 import {
   BrowserRouter as Router,
   Routes,
@@ -28,32 +30,56 @@ class App extends React.Component {
     })
   }
 
-
   render() {
-    return() {
-         /* <Nav/> */}
-      // <Router>
-        // <Login/>
-        // <Profile />
-        // <Logout/> */}
-        // {<Routes>
-          // <Route 
-          //   exact path="/"
-          //   element={<DropDownList updateSelectedPark={this.updateSelectedPark} />}
-          // >
-          // </Route>
-          // <Route
-          //   exact path="/ParkData"
-          //   element={<ParkData selectedPark={this.state.selectedPark} />}
-          // >
-          // </Route>
-          // <Route
-          //   exact path="/About"
-          //   element={<About />}
-          // >
-          // </Route>
-        // </Routes>
-      // </Router>
-// }
-
-export default App
+      return(
+        <>
+           {/* <Nav/> */}
+        <Router>
+          {/* <Login/>
+          <Profile />
+          
+          // // <Footer />
+        <Logout/> */}
+            <Nav/>
+          <Routes>
+            <Route 
+              exact path="/"
+              element={<DropDownList updateSelectedPark={this.updateSelectedPark} 
+              />}
+              >
+            </Route>
+            <Route
+              exact path="/ParkData"
+              element={<ParkData selectedPark={this.state.selectedPark} />}
+              >
+            </Route>
+            <Route
+              exact path="/About"
+              element={<About />}
+              >
+            </Route>
+          </Routes>
+          {/* <Footer/> */}
+        </Router>
+        {/* <div className='hero-container'> */}
+        {/* <h1>WELCOME TO YOUR NATIONAL PARKS</h1>
+      <p>SEARCH DIRECTORY</p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
+        </Button>
+        </div>
+      </div>
+        <div className='page-container'>
+      {/* <Footer/> */}
+    {/* </div> */} */}
+      </>
+      )
+    }
+  }
+  
+  export default App
